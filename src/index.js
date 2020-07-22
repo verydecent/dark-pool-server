16dc2c1d85673a28c05a5488138ab80179e46a36
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-21 23:32:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-/* const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-
-mongoose.
-  connect(process.env.MONGO_URI, { userNewUrlParser: true })
-  .then(() => console.log('Database Connected'));
-
-mongoose.connection.on('error', err => {
-  console.log(`Database connection error ${err.message}`);
-}); */
-
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
@@ -19,6 +7,7 @@ import models, { connectDb } from './models';
 const app = express();
 
 // Application level middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,9 +18,48 @@ app.get('/', (req, res) => {
   res.send('API success');
 });
 
-connectDb()
-  .then(async () => {
-    app.listen(process.env.PORT, () =>
-      console.log(`App listening on Port ${process.env.PORT}`)
-    );
+// Reinitialize database if set to true
+
+const eraseDatabaseOnSync = true;
+
+connectDb().then(async () => {
+  if (eraseDatabaseOnSync) {
+    await Promise.all([
+      models.User.deleteMany({}),
+      models.Task.deleteMany({})
+    ]);
+  }
+
+  createUsersWithTasks();
+
+  app.listen(process.env.PORT, () =>
+    console.log(`App listening on Port ${process.env.PORT}`));
+  }
+);
+
+// Seeds
+
+const createUsersWithTasks = async () => {
+  const user1 = new models.User({
+    username: 'admin'
   });
+
+  const task1 = new models.Task({
+    text: 'Read Flower for Algernon',
+    user: user1.id
+  });
+
+  const user2 = new models.User({
+    username: 'user'
+  });
+
+  const task2 = new models.Task({
+    text: 'Read Flower for Algernon',
+    user: user2.id
+  });
+
+  await task1.save();
+  await user1.save();
+  await task2.save();
+  await user2.save();
+}
