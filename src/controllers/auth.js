2970e352db8e0d14cb929c4bd5057caccb34ebcd
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-30 22:47:17 -0700

    update to controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,21 @@
 export const register = (req, res) => {
+  try {
+    const { email, password, passwordConfirm, username } = req.body;
+
+    if (!email || !password || !passwordConfirm) {
+      return res.status(400).json({ message: 'Missing one or more required fields' });
+    }
+
+    if (passowrd.length < 5) {
+      return res.status(400).json({ message: 'The password needs to be at least 5 characters long' });
+    }
+    
+    if (password !== passwordConfirm) {
+      return res.status(400).json({ message: 'The password and password confirm do not match' });
+    }
+  }
+  catch (err) {
+   res.status(500).json(err); 
+  }
   return res.json({ data: 'Register endpoint from controllers folder' });
 };
