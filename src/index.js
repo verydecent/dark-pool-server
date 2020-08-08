592e2f929f0c255f383b2f34c71e2b4a758ef45c
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-08 02:41:07 -0700

    implement morgan for http request logging

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import morgan from 'morgan';
 import models, { connectDb } from './models';
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 // Application level middleware
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
