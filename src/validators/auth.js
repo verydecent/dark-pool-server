936cbe0629c27fa45dd4bbacc3106a0c25df2ed7
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-08 06:05:09 -0700

    code changes

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -2,13 +2,13 @@ import { check } from 'express-validator';
 
 export const userRegistrationValidator = [
   check('username')
-  .not()
-  .isEmpty()
-  .withMessage('Username is required'),
+    .not()
+    .isEmpty()
+    .withMessage('Username is required'),
   check('email')
-  .isEmail()
-  .withMessage('Must be a valid Email Address'),
+    .isEmail()
+    .withMessage('Must be a valid Email Address'),
   check('password')
-  .isLength({ min: 6 })
-  .withMessage('Password must be at least 6 characters long')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long')
 ];                            
