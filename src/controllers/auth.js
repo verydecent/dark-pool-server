22531e18183c3b0ea0ead90e740478b1fde8dfec
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-08 06:05:58 -0700

    Implement registration controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,32 @@
 import models from '../models';
 
+
 export const register = (req, res) => {
+
+  const { username, email, password } = req.body;
+
+  models.User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: 'User with provided Email already exists'
+      });
+    }
+  });
+
+  const newUser = new models.User({ username, email, password });
+
+  newUser.save((err, success) => {
+    if (err) {
+      console.log('User Registration Error', err);
+      return res.status(400).json({
+        error: err
+      });
+    }
+
+    res.json({
+      message: 'User Registration Success'
+    });
+  });
   // Create user id
   // username
   // email
