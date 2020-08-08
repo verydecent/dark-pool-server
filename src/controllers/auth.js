61cf2478bb962d83c98ba702b2ea3d97a34e36ad
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-08 04:04:56 -0700

    Update validators as middleware

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,16 +8,14 @@ export const register = (req, res) => {
 
   console.log('====auth====', req.body);
 
-  const newUser = new models.User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  });
+  // const newUser = new models.User({
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: req.body.password
+  // });
 
-  console.log('newUser', newUser);
-
-  newUser.save(function(err, doc) {
-    if (err) return res.status(500).json(err);
-    res.status(200).json(doc);
-  });
+  // newUser.save(function(err, doc) {
+  //   if (err) return res.status(500).json(err);
+  //   res.status(200).json(doc);
+  // });
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import models from '../models';
 import { register } from '../controllers/auth';
+import { userRegistrationValidator } from '../validators/auth';
+import { runValidation } from '../validators';
 
 const router = Router();
 
@@ -8,6 +10,6 @@ router.get('/', (req, res) => {
   res.json({ message: 'Auth Route Get Success' });
 });
 
-router.post('/register', register);
+router.post('/register', userRegistrationValidator, runValidation, register);
 
 export default router;
