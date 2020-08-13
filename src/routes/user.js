a29ec5397d3488c636b2ad3af2dae9e1707b50c4
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-13 10:20:49 -0700

    Add comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { readUser, updateUser } from '../controllers/user';
-import { requireLogin } from '../controllers/auth';
+import { requireLogin, adminMiddleware } from '../controllers/auth';
 
 const router = Router();
 
+// Retrieve user info based on user id
 router.get('/:id', requireLogin, readUser);
 
-router.put('/:id', requireLogin, updateUser);
+// Update Subscriber user info
+router.put('/', requireLogin, updateUser);
+
+// Update Admin user info
+router.put('/admin/update', requireLogin, adminMiddleware, updateUser);
 
 export default router;
