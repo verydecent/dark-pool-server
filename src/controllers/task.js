ee1a5338f3c6e39ec16eaaf9a1898acc5f90d285
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-24 03:01:15 -0700

    comments for task routes and updated so that we are using moment and query strings to get tasks by date

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,8 +1,39 @@
 import models from '../models';
+import mongoose from 'mongoose';
+
+export const readtTaskByDate = (req, res) => {
+  // Destructure request body
+
+  // Consider how we will get the beginning and end of the date we're retreiving
+
+  // Receive date, create middleware that grabs current date and then gets the beginning time and end time of that specified date?
+  // Or resolve logic in front end? No I don't want anything controlled on the front end
+  // We will do the logic here
+
+  // Create route that works for componentDidMount on day slection? Or create one that is dynamic where the date is sent from the front end?
+
+
+
+  const { user_id, date } = req.body;
+
+  models.Task.find({ user_id, date })
+}
 
 export const readTask = (req, res) => {
-  models.Task.find({ user_id: req.params.user_id }, function (err, doc) {
+  console.log('req.query ===> ', req.query);
+  console.log('req.params.user_id ===> ', req.params.user_id);
+
+  // Have to use mongoose.Types.ObjectId if you want to use find with an ObjectId
+
+  models.Task.find({
+    user_id: mongoose.Types.ObjectId(req.params.user_id),
+    createdAt: {
+      $gte: req.query.start_date,
+      $lte: req.query.end_date
+    }
+  }, function (err, doc) {
     if (err) res.status(500).json(err);
+    console.log('doc ===> ', doc);
     res.status(200).json(doc);
   });
 }
