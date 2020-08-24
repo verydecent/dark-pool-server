00661b80d529cc8bf7a0d5eb735996b0e7e4eb85
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-24 00:28:14 -0700

    comma

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,21 +1,21 @@
 import models from '../models';
 
 export const readTask = (req, res) => {
-  models.Task.find({ user_id: req.params.user_id }, function(err, doc) {
-      if (err) res.status(500).json(err);
-      res.status(200).json(doc);
-  })
+  models.Task.find({ user_id: req.params.user_id }, function (err, doc) {
+    if (err) res.status(500).json(err);
+    res.status(200).json(doc);
+  });
 }
 
 export const createTask = (req, res) => {
   console.log('POST request for TASK', req.body);
   const newTask = new models.Task({
-      user_id: req.body.user_id,
-      title: '',
-      description: ''
+    user_id: req.body.user_id,
+    title: '',
+    description: ''
   });
-  
-  newTask.save(function(err, doc) {
+
+  newTask.save(function (err, doc) {
     if (err) return res.status(500).json(err);
     res.status(200).json(doc);
   });
@@ -24,7 +24,7 @@ export const createTask = (req, res) => {
 export const updateTask = (req, res) => {
   console.log('PUT request for TASK', req.body);
 
-  models.Task.findByIdAndUpdate(req.params.task_id, req.body, { new: true }, function(err, doc) {
+  models.Task.findByIdAndUpdate(req.params.task_id, req.body, { new: true }, function (err, doc) {
     if (err) res.status(500).json(err);
     console.log('~~~ PUT document ~~~');
     res.status(200).json(doc);
@@ -33,10 +33,10 @@ export const updateTask = (req, res) => {
 
 export const deleteTask = (req, res) => {
   console.log('task delete route', req.params.task_id);
-  models.Task.findOne({ _id: req.params.task_id }, function(err, doc) {
+  models.Task.findOne({ _id: req.params.task_id }, function (err, doc) {
     if (err || !doc) res.status(400).json(err);
 
-    doc.remove(function(err, doc) {
+    doc.remove(function (err, doc) {
       if (err) res.status(500).json(err);
       res.status(200).json(doc);
     });
